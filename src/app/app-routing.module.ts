9486f90c06c4393b1f58526420e0af58d4fec26c
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-01 22:51:49 -0300

    Criando rota para tecnico list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children:[//Quando o path for vazio será renderizado na tela o componente NavComponent
-      {path: 'home', component: HomeComponent}
+      {path: 'home', component: HomeComponent},//Quando a rota for home será renderizada a tela home
+      {path: 'tecnicos', component: TecnicoListComponent}//Quando a rota for tecnicos será renderizada a tela tecnicos
     ]
   }
 ];
