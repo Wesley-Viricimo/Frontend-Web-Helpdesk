a01cff37b0272843477513b5f97b8c7844d3ffe7
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-20 22:24:02 -0300

    Implementação da criação de novo técnico

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,16 +12,51 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class TecnicoCreateComponent implements OnInit {
 
+  tecnico: Tecnico ={
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe(() => {
+      this.toast.success('Tecnico cadastrado com sucesso', 'Cadastro');
+      this.router.navigate(['tecnicos']);
+    }, exception => {
+      if(exception.error.errors) {
+        exception.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(exception.error.message);
+      }
+    })
   }
 
+  addPerfil(perfil: any): void { //Método responsável por adicionar perfis
+    if(this.tecnico.perfis.includes(perfil)) { //Se a lista de perfil já contiver o perfil que foi selecionado
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1); //Irá remover o perfil buscando pelo perfil que foi informado e remover apenas o primeiro caso que encontrar
+    } else {
+      this.tecnico.perfis.push(perfil); //Se perfil ainda não existir significa que ainda não existe na lista
+    }
+  }
+  
   validaCampos(): boolean {
     return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid;
   }
--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -16,4 +16,8 @@ export class TecnicoService {
   findAll(): Observable<Tecnico[]> { //Método responsável por fazer a requisição e retornar os técnicos, e irá retornar um observable de uma lista de tecnicos
     return this.http.get<Tecnico[]>(`${API_CONFIG.baseURL}/tecnicos`);
   }
+
+  create(tecnico: Tecnico): Observable<Tecnico> { //Método responsável por criar um tecnico, irá receber um tecnico como parâmetro e irá retornar um observable de técnico para que seja feita a requisição e seja observada a resposta
+    return this.http.post<Tecnico>(`${API_CONFIG.baseURL}/tecnicos`, tecnico);
+  }
 }
