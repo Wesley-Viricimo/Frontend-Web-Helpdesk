9ef2d754cc2df8329b8e71d875b052d65d379cd7
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-24 16:09:03 -0300

    Ajuste final no componente login

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/update/1'])//Quando projeto for iniciado, irá navegar para a rota home
+    this.router.navigate(['home'])//Quando projeto for iniciado, irá navegar para a rota home
   }
 
   Logout() { //Quando usuário selecionar a opção Logout no menu
